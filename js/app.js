0451f7032d0072f10bda827513f50203f65151a0
farid95g <f95guluzade@gmail.com>
2020-11-16 15:40:48 +0400

    progress bar layout and design

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// input field containers
+const containers = document.querySelectorAll("div.progress-step");
+
+// previous and next buttons
+const previous = document.getElementById("previousStep"),
+      next = document.getElementById("nextStep");
+
+
+
+/** Testing Area Below **/
+console.log(containers);
