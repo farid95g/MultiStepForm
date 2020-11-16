ad81daeaf6fa9a111e2757d65d5db6b89ce97ba8
farid95g <f95guluzade@gmail.com>
2020-11-16 17:43:39 +0400

    dynamic progress bar on step change

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// progress bar
+const progressBar = document.querySelector(".progress");
+
 // input field containers
 const containers = document.querySelectorAll("div.progress-step");
 
@@ -5,7 +8,29 @@ const containers = document.querySelectorAll("div.progress-step");
 const previous = document.getElementById("previousStep"),
       next = document.getElementById("nextStep");
 
+// preventing form reloading the poge on submit
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+});
+
+// next button's click event
+next.addEventListener("click", () => {
+  // progress bar change
+  if (Number(progressBar.style.width.split('%')[0]) < 100) {
+    progressBar.style.width = `${Number(progressBar.style.width.split('%')[0]) + 25}%`;
+  }
+});
+
+// previous button's click event
+previous.addEventListener("click", () => {
+  // progress bar change
+  if (Number(progressBar.style.width.split('%')[0]) >= 25) {
+    progressBar.style.width = `${Number(progressBar.style.width.split('%')[0]) - 25}%`;
+  }
+});
+
+
 
 
 /** Testing Area Below **/
-console.log(containers);
+// console.log(Number(progressBar.style.width.split('%')[0]));
